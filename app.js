a457992361680ababf44e246d9b17d8d26ceb72b
Yukio Ez <67267969+YukioEz@users.noreply.github.com>
2020-08-01 23:14:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const navSlide = () => {
     const burger = document.querySelector(".burger")
     const nav = document.querySelector(".nav-links")
     const navlinks = document.querySelectorAll(".nav-links li")
-
+    const navactive = document.querySelector(".nav-links.nav-active" )
     burger.addEventListener("click",() => {
         nav.classList.toggle("nav-active")
+        navactive.style.display = "block" 
+        
     });
     navlinks.forEach((link, index)=>{
         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}`
