fea5f7754109a49a4f267f634700b0c7bf80315b
Yukio Ez <67267969+YukioEz@users.noreply.github.com>
2020-08-07 18:58:58 +0200

    Stuff

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const navSlide = () => {
     const burger = document.querySelector(".burger")
     const nav = document.querySelector(".nav-links")
-    const navlinks = document.querySelectorAll(".nav-links li")
    
     burger.addEventListener("click",() => {
         nav.classList.toggle("nav-active")
