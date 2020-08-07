957d4d52a2bae5ab5f3c453ea88cfe7bbd7457c2
Yukio Ez <67267969+YukioEz@users.noreply.github.com>
2020-08-07 18:51:22 +0200

    Removed animation

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ const navSlide = () => {
 
         
     });
-    navlinks.forEach((link, index)=>{
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}`
-    });
 }
 
 navSlide();
