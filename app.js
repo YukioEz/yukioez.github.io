2632838b564a042b06fcb4115b1cc5585aaa2c7d
Yukio Ez <67267969+YukioEz@users.noreply.github.com>
2020-08-09 17:17:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 const navSlide = () => {
     const burger = document.querySelector(".burger")
     const nav = document.querySelector(".nav-links")
-    let active = false
     
-    function open(){
-        nav.classList.toggle('open')
-    }
-    function close(){
-        nav.classList.toggle('close')
-    }
+    
     burger.addEventListener("click",() => {
         nav.classList.toggle('nav-active open close')
     });
