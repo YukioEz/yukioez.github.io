7485cf66ee0e490788d0110f4d21d24bf69775cc
Yukio Ez <67267969+YukioEz@users.noreply.github.com>
2020-08-09 16:56:24 +0200

    a

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const navSlide = () => {
    
     burger.addEventListener("click",() => {
         nav.classList.toggle("nav-active")
-
+        if(nav.classList.contains("nav-active")){
+            nav.classList.add('close')
+            nav.classList.remove('close')
+        }else{
+            nav.classList.add('open')
+            nav.classList.remove('open')
+        }
     });
 }
 
